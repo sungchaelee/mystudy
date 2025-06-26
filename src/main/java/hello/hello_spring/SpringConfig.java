fcabb5ac5e69d5601427a765e96d6f00ecbdaae4
sungchae <sungge0661@naver.com>
2025-06-26 18:15:05 +0900

    fin

---
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -0,0 +1,31 @@
+package hello.hello_spring;
+
+import hello.hello_spring.repository.JpaMemberRepository;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+import hello.hello_spring.service.MemberService;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    private final MemberRepository memberRepository;
+
+    public SpringConfig(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        // return new MemoryMemberRepository();
+//        return new JpaMemberRepository(em);
+//    }
+}
